c05bd8a2693a1bf8b824ce74974f3b9668621086
chrispflepsen <cpflepsen@gmail.com>
2022-12-06 16:07:44 -0600

    Fixed setup

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew bundle
 
 #zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-cp zshrc ~/.zshrc
+mv zshrc ~/.zshrc
 
 #install .vimrc
 git clone --depth=1 https://github.com/amix/vimrc.git ~/.vim_runtime
